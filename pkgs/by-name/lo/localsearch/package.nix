2fba346bd45f6c106151b53a9d6ce279809f5e76
Jan Tojnar <jtojnar@gmail.com>
2025-05-06 01:11:55 +0200

    fixup! localsearch: 3.9.rc → 3.9.0
    revert so that we can apply it to previous commit

--- pkgs/by-name/lo/localsearch/package.nix
+++ pkgs/by-name/lo/localsearch/package.nix
@@ -19,11 +19,11 @@
   bzip2,
   dbus,
   exempi,
-  ffmpeg,
   giflib,
   glib,
   gobject-introspection,
   gnome,
+  gst_all_1,
   icu,
   json-glib,
   libcue,
@@ -39,6 +39,7 @@
   libtiff,
   libuuid,
   libxml2,
+  networkmanager,
   poppler,
   systemd,
   taglib,
@@ -86,11 +87,16 @@ stdenv.mkDerivation (finalAttrs: {
       bzip2
       dbus
       exempi
-      ffmpeg
       giflib
       gexiv2
       totem-pl-parser
       tinysparql
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-ugly
+      gst_all_1.gstreamer
+      gst_all_1.gst-libav
       icu
       json-glib
       libcue
@@ -110,6 +116,7 @@ stdenv.mkDerivation (finalAttrs: {
     ]
     ++ lib.optionals stdenv.hostPlatform.isLinux [
       libseccomp
+      networkmanager
       systemd
       upower
     ]
